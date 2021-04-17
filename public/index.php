9b5e387962936e2393df2423975138e643520772
philomagi <tooppoo56@gmail.com>
2021-04-17 22:21:29 +0900

    add libraries

--- public/index.php
+++ public/index.php
@@ -1,4 +1,24 @@
 <?php
-foreach (range(1, 10) as $i) {
-  echo "Hello World($i)!<br/>";
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$whoops = new \Whoops\Run();
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+$psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+
+$creator = new \Nyholm\Psr7Server\ServerRequestCreator(
+  $psr17Factory,
+  $psr17Factory,
+  $psr17Factory,
+  $psr17Factory
+);
+
+$serverRequest = $creator->fromGlobals();
+$path = $serverRequest->getUri()->getPath();
+
+if ($path === '/') {
+  echo date('Y年m月d日 H時i分s秒');
 }
