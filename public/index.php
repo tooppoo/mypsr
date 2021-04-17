ce064ee7960b4d6924a5fab1ff26a49b5bb19922
philomagi <tooppoo56@gmail.com>
2021-04-17 22:36:20 +0900

    add script

--- public/index.php
+++ public/index.php
@@ -1,22 +1,24 @@
 <?php
+
 require __DIR__ . '/../vendor/autoload.php';
 
 error_reporting(E_ALL);
 
-$whoops = new \Whoops\Run();
+$whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
 $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
 
 $creator = new \Nyholm\Psr7Server\ServerRequestCreator(
-    $psr17Factory,
-    $psr17Factory,
-    $psr17Factory,
-    $psr17Factory
+    $psr17Factory, // ServerRequestFactory
+    $psr17Factory, // UriFactory
+    $psr17Factory, // UploadedFileFactory
+    $psr17Factory  // StreamFactory
 );
 
 $serverRequest = $creator->fromGlobals();
+
 $path = $serverRequest->getUri()->getPath();
 
 if ($path === '/') {
