33da1f83ae2ca85eb20c80421b5d1e33c6a25319
philomagi <tooppoo56@gmail.com>
2021-04-17 22:31:50 +0900

    apply fixer

--- public/index.php
+++ public/index.php
@@ -10,15 +10,15 @@ $whoops->register();
 $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
 
 $creator = new \Nyholm\Psr7Server\ServerRequestCreator(
-  $psr17Factory,
-  $psr17Factory,
-  $psr17Factory,
-  $psr17Factory
+    $psr17Factory,
+    $psr17Factory,
+    $psr17Factory,
+    $psr17Factory
 );
 
 $serverRequest = $creator->fromGlobals();
 $path = $serverRequest->getUri()->getPath();
 
 if ($path === '/') {
-  echo date('Y年m月d日 H時i分s秒');
+    echo date('Y年m月d日 H時i分s秒');
 }
